99413c54bdf26ab369704d273e7679936a27f9e0
Vu Cong Anh <conganhyh5411@gmail.com>
2023-10-17 15:30:03 +0700

    Cấu hình Router/Layout cho dự án

--- src/myAlert.js
+++
@@ -1,5 +0,0 @@
-const myAlert = () => {
-  alert("Say đmm");
-};
-
-export default myAlert;
---
+++ src/routes/index.js
@@ -0,0 +1,18 @@
+import { HeaderOnly } from "~/components/Layout";
+import Home from "~/pages/Home";
+import Following from "~/pages/Following";
+import Profile from "~/pages/Profile";
+import Upload from "~/pages/Upload";
+import Search from "~/pages/Search";
+
+const publicRoutes = [
+  { path: "/", compoment: Home },
+  { path: "/following", compoment: Following },
+  { path: "/profile", compoment: Profile },
+  { path: "/upload", compoment: Upload, layout: HeaderOnly },
+  { path: "/search", compoment: Search, layout: null },
+];
+
+const privateRoutes = [];
+
+export { publicRoutes, privateRoutes };
